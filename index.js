5ab6c268da6432e869a47c0c2cdf346e247e11eb
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-05-15 19:31:31 +0300

    extracting js embed code

---
+++ index.js
@@ -0,0 +1,9 @@
+
+      // <!--
+      document.onReady(function() {
+        var game = new TypeNinja('game-container');
+      });
+      // -->
+    
+            var i,y,x="3c6120687265663d226d61696c746f3a696e666f4072696768746a732e6f7267223e436f6e746163743c2f613e";y='';for(i=0;i<x.length;i+=2){y+=unescape('%'+x.substr(i,2));}document.write(y);
+          
